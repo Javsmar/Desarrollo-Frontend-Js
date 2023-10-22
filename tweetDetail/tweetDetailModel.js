2ac3d440cc2d1e6f33e1ff43bd241015ad16867f
Javier <mastos44@gmail.com>
2023-10-22 13:22:20 +0200

    create sparrestApi to refactor API requests

--- tweetDetail/tweetDetailModel.js
+++ tweetDetail/tweetDetailModel.js
@@ -1,56 +1,25 @@
+import { sparrestApi } from "../utils/sparrestApi.js";
+
 const parseTweet = (tweet) => {
     return {
         handler: tweet.user.username,
         message: tweet.message,
         likes: [],
-        userId: tweet.user.id, 
+        userId: tweet.user.id,
         id: tweet.id
     }
-};
+}
 
 export const getTweet = async (tweetId) => {
-    const url = (`http://localhost:8000/api/tweets/${tweetId}?_expand=user`);
-    let tweet;
+    const endpoint = `api/tweets/${tweetId}?_expand=user`;
 
-    try {
-        const response = await fetch(url);
-        if(response.ok){
-            tweet = await response.json();
-
-        }else{
-            throw new Error('El tweet no existe');
-        }
-        
-    } catch (error) {
-        throw error.message;
-    }
+    const tweet = await sparrestApi().get(endpoint)
 
     return parseTweet(tweet);
 }
 
 export const deleteTweet = async (tweetId) => {
-    const url = `http://localhost:8000/api/tweets/${tweetId}`;
-    const token = localStorage.getItem('token');
-  
-    let response;
-    try {
-      response = await fetch(url, {
-        method: "DELETE",
-        headers: {
-          'Content-type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        }
-      });
-  
-      if (!response.ok) {
-        const data = await response.json()
-        throw new Error(data.message);
-      }
-    } catch (error) {
-      if (error.message) {
-        throw error.message;
-      } else {
-        throw error;
-      }
-    }
-  }
+    const endpoint = `api/tweets/${tweetId}`;
+
+    await sparrestApi().delete(endpoint);
+}
