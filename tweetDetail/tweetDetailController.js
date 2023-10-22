57f0ab7bef3add70d7e7ec2c4ab1f3ba3c3d4ef3
Javier <mastos44@gmail.com>
2023-10-22 12:21:29 +0200

    show remove button when authenticated user is the owner of the tweet

--- tweetDetail/tweetDetailController.js
+++ tweetDetail/tweetDetailController.js
@@ -1,12 +1,14 @@
 import { getTweet } from "./tweetDetailModel.js"
 import { buildTweet } from "./tweetDteail.Views.js";
 import {dispatchEvent} from "../utils/dispatchEvent.js"
+import { decodeToken } from "../utils/decodeToken.js";
+import {deleteTweet} from "./tweetDetailModel.js"
 
 export const tweetDetailController = async (tweetDetail, tweetId) => {
     try {
         const tweet = await getTweet(tweetId);
         tweetDetail.innerHTML = buildTweet(tweet);
-
+        handleDeleteTweet(tweet, tweetDetail);
     } catch (error) {
         
         dispatchEvent('tweetLouder', {type: "error", message: "tweet no existe"}, tweetDetail);
@@ -14,6 +16,28 @@ export const tweetDetailController = async (tweetDetail, tweetId) => {
             window.location = './index.html';
         }, 3000)
     }
-    
 }
 
+const handleDeleteTweet = (tweet, tweetDetail) => {
+    const token  = localStorage.getItem('token');
+    if(token){
+        const {userId} = decodeToken(token);
+        if(userId === tweet.userId){
+            addDeletetweetButton(tweet, tweetDetail);
+        }
+    }
+}
+
+const addDeletetweetButton = (tweet, tweetDetail) => {
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Borrar Tweet';
+
+    deleteButton.addEventListener('click', async () => {
+        if(confirm('¿Seguro que quieres borra el tweet')){
+            await deleteTweet(tweet.id);
+            window.location = "index.html";
+        }
+    })
+
+    tweetDetail.appendChild(deleteButton);
+}
--- tweetDetail/tweetDetailModel.js
+++ tweetDetail/tweetDetailModel.js
@@ -2,7 +2,9 @@ const parseTweet = (tweet) => {
     return {
         handler: tweet.user.username,
         message: tweet.message,
-        likes: []
+        likes: [],
+        userId: tweet.user.id, 
+        id: tweet.id
     }
 };
 
@@ -25,3 +27,30 @@ export const getTweet = async (tweetId) => {
 
     return parseTweet(tweet);
 }
+
+export const deleteTweet = async (tweetId) => {
+    const url = `http://localhost:8000/api/tweets/${tweetId}`;
+    const token = localStorage.getItem('token');
+  
+    let response;
+    try {
+      response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          'Content-type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+  
+      if (!response.ok) {
+        const data = await response.json()
+        throw new Error(data.message);
+      }
+    } catch (error) {
+      if (error.message) {
+        throw error.message;
+      } else {
+        throw error;
+      }
+    }
+  }
